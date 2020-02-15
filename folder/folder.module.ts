e3c3c499f4d06de1211cb743f26869c1e856f3d0
Serena Sensini <serena.sensini@gmail.com>
2020-02-15 19:28:18 +0100

    CODE: input

--- folder/folder.module.ts
+++ folder/folder.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { FolderPageRoutingModule } from './folder-routing.module';
 import { FolderPage } from './folder.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    FolderPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        FolderPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [FolderPage]
 })
 export class FolderPageModule {}
--- folder/folder.page.ts
+++ folder/folder.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {AlertController} from '@ionic/angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-folder',
@@ -9,24 +10,54 @@ import {AlertController} from '@ionic/angular';
 })
 export class FolderPage implements OnInit {
   public folder: string;
+  cittaPartenza: string;
+  cittaArrivo: string;
+  cittaRitornoDisab = true;
 
-  constructor(private activatedRoute: ActivatedRoute, private alertController: AlertController) { }
+  form: FormGroup;
+  submitted = false;
+
+
+  constructor(private activatedRoute: ActivatedRoute, private alertController: AlertController, private formBuilder: FormBuilder, ) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    this.form = this.formBuilder.group({
+      cittaPartenza: ['', Validators.required],
+      cittaRitorno: [''],
+      dataPartenza: ['', [Validators.required]],
+      dataRitorno: ['']
+    });
+
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.form.value));
   }
 
   async presentAlert() {
     const alert = await this.alertController.create({
       header: 'Avviso',
       subHeader: 'Avviso di apertura',
-      message: 'Questo messaggio viene visualizzato quando entri nel componente' + this.folder,
+      message: 'Questo messaggio viene visualizzato quando apri l\'app.',
       buttons: ['OK']
     });
-
     await alert.present();
   }
 
-
+  setDisabled() {
+    if (this.cittaPartenza.length !== 0) {
+      this.cittaRitornoDisab = false;
+    }
+  }
 
 }
